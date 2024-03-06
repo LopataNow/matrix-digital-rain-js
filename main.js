6502bf17ac081d74dd6d5665bcbee11d3afa265d
Peter Kopáč <lopata.kopac@gmail.com>
2024-03-06 12:24:37 +0100

    refactor change backgroun color to clear

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import './style.css';
 import p5 from 'p5';
 
-const symbolSize = 16;
+const symbolSize = 14;
 const spaceBetweenSymbols = 7;
 
 class StreamsManager{
   streams = [];
+  
   generateStreams() {
     for (let index = 0; index < window.innerWidth / symbolSize; index++) {
       this.streams.push(new Stream({
         x: index * symbolSize,
-        y: Math.random() * -window.innerHeight,
-        speed: 3 + Math.random() * 20,
+        y: Math.random() * -window.innerHeight / 3,
+        speed: 6 + Math.random() * 20,
         sybolsCount: Math.round(6 + Math.random() * 14),
         opacity: Math.round(155 + Math.random() * 100)
       }));
@@ -33,13 +34,15 @@ class StreamsManager{
 class Stream {
   symbols = [];
   intervals = [];
+
   constructor({x = 0, y = 0, speed = 5, opacity = 255, sybolsCount = 5}) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.opacity = opacity;
     this.symbolsCount = (sybolsCount > 0) ? sybolsCount : 5;
-    this.leght = this.symbolsCount  *  (symbolSize + spaceBetweenSymbols);
+    this.symbolSize = Math.round(10 + Math.random() * (symbolSize - 10));
+    this.leght = this.symbolsCount  *  this.symbolSize;
     for (var index = 0; index <= this.symbolsCount; index++) {
       this.symbols.push('');
       this.intervals.push(0);
@@ -75,7 +78,7 @@ class Stream {
   update(p) {
     this.y += this.speed  * p.deltaTime * 0.01;
     if (this.y > window.innerHeight + this.leght) {
-      this.y = 0;
+      this.y = Math.random() * -window.innerHeight / 3;
     }
 
     this.generateUpdateSymbols(p);
@@ -89,7 +92,7 @@ class Stream {
     for (var index = 1; index < this.symbols.length; index++) {
       color.setAlpha(this.opacity - index * 12);
       p.fill(color);
-      p.text(this.symbols[index], this.x, this.y - index * symbolSize + spaceBetweenSymbols);
+      p.text(this.symbols[index], this.x, this.y - index * symbolSize);
     }
   }
 }
@@ -99,18 +102,22 @@ new p5(p => {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noStroke();
 
     p.textFont('Consolas');
     p.textSize(symbolSize);
 
     streamsManager.generateStreams();
-    p.frameRate(60);
+    p.frameRate(30);
   }
 
-  p.draw = () => {
-    p.background(0, 150);
-    
+  p.draw = () => {    
     streamsManager.updateStreams(p);
+    p.clear();
     streamsManager.drawStreams(p);
+  }
+
+  p.windowResized = () => {
+    resizeCanvas(windowWidth, windowHeight);
   }
 });
